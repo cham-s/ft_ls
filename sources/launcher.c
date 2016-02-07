1501f919f62391da281abf9e2076ae1292f32303
Chamsidine ATTOUMANI <cattouma@e1r9p19.42.fr>
2016-02-07 09:49:50 +0100

    more tests passed

--- sources/launcher.c
+++ sources/launcher.c
@@ -6,7 +6,7 @@
 /*   By: cattouma <cattouma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/29 14:26:40 by cattouma          #+#    #+#             */
-/*   Updated: 2016/02/03 17:53:56 by cattouma         ###   ########.fr       */
+/*   Updated: 2016/02/07 09:41:35 by cattouma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	ft_list(char *directory, char *options, int ac, char **av)
 {
     static int i = 0;
 
+	// a virer?
     av++;
     ac++;
 	if (ft_strchr(options, 'R'))
@@ -37,6 +38,28 @@ void	ft_list(char *directory, char *options, int ac, char **av)
 	else
         listdir(directory, options);
     i++;
+}
+
+static void		checkfile(t_file *list, int (*fstat)(const char *name, struct stat *buf))
+{
+
+	struct stat file;
+
+	if (fstat((list)->filename, &file) < 0)
+	{
+		ft_perror((list)->filename);
+		if ((list)->next)
+		{
+			stat((list)->next->filename, &file);
+			if (S_ISDIR(file.st_mode))
+				printdirnl((list)->next->filename, true);
+		}
+	}
+	else if (!S_ISDIR(file.st_mode))
+	{
+		ft_putendl((list)->filename);
+		nlafterfile(list);
+	}
 }
 
 void	apply_ft_list(t_file **list, char *options, int ac, char **av)
@@ -54,20 +77,9 @@ void	apply_ft_list(t_file **list, char *options, int ac, char **av)
 		{
 			tmp = *list;
 			if (stat((*list)->filename, &file) < 0)
-            {
-				ft_perror((*list)->filename);
-                if ((*list)->next)
-                {
-                    stat((*list)->next->filename, &file);
-                    if (S_ISDIR(file.st_mode))
-                        printdirnl((*list)->next->filename, true);
-                }
-            }
-            else if (S_ISREG(file.st_mode))
-            {
-                ft_putendl((*list)->filename);
-                nlafterfile(*list);
-            }
+				checkfile(*list, lstat);
+			else if (S_ISREG(file.st_mode))
+				checkfile(*list, stat);
 			else
             {
                 if ((*list)->next != NULL && i == 0)
