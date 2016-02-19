84e65f0c4e38383976b86181711917de6337cd74
Chamsidine ATTOUMANI <cattouma@e1r9p19.42.fr>
2016-02-19 19:39:41 +0100

    display.c norm ok

--- sources/display.c
+++ sources/display.c
@@ -6,7 +6,7 @@
 /*   By: cattouma <cattouma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/17 14:31:36 by cattouma          #+#    #+#             */
-/*   Updated: 2016/02/19 18:04:45 by cattouma         ###   ########.fr       */
+/*   Updated: 2016/02/19 19:37:33 by cattouma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ void	apply_merge(t_file **list, char *options)
 {
 	if (*list == NULL)
 		return ;
-
 	if (ft_strchr(options, 'r'))
 		if (ft_strchr(options, 't'))
 		{
@@ -45,22 +44,6 @@ void	apply_merge(t_file **list, char *options)
 		else
 			ft_lstmergesort(list, "");
 	}
-    /*  */
-	/* if (ft_strchr(options, 't')) */
-	/* { */
-	/* 	if (ft_strchr(options, 'r')) */
-	/* 	{ */
-	/* 		ft_lstmergesort(list, ""); */
-	/* 		ft_lstmergesort(list, "tr"); */
-	/* 	} */
-	/* 	else */
-	/* 		ft_lstmergesort(list, ""); */
-	/* 	ft_lstmergesort(list, "t"); */
-	/* } */
-	/* else if (ft_strchr(options, 'r')) */
-	/* 	ft_lstmergesort(list, "r"); */
-	/* else */
-	/* 	ft_lstmergesort(list, ""); */
 }
 
 void	getfiles(char *filename, t_file **list, char *options, t_max *maxs)
@@ -87,63 +70,53 @@ void	getfiles(char *filename, t_file **list, char *options, t_max *maxs)
 
 int		check_for_a(t_file *current, char *options)
 {
-	if (OPTIN(options, 'a') == NULL && 
+	if (OPTIN(options, 'a') == NULL &&
 		TRIM(current->filename)[0] == '.' &&
 		OPTIN(options, 'A') == NULL)
-	{
-		current = current->next;
 		return (1);
-	}
 	else if (OPTIN(options, 'A') && OPTIN(options, 'a') == NULL)
 	{
 		if (TRIM(current->filename) == "." ||
 			TRIM(current->filename) == "..")
-		{
-			current = current->next;
 			return (1);
-		}
 	}
 	return (0);
+}
+
+void	compute_total(t_file **list, int *result,
+		struct stat *file, char *options)
+{
+	t_file		*current;
+
+	current = *list;
+	while (current != NULL)
+	{
+		if (check_for_a(current, options))
+		{
+			current = current->next;
+			continue ;
+		}
+		if (lstat(current->filename, file) < 0)
+			return ;
+		if (S_ISLNK(file->st_mode))
+			*result += file->st_blocks;
+		else
+		{
+			if (stat(current->filename, file) < 0)
+				return ;
+			*result += file->st_blocks;
+		}
+		current = current->next;
+	}
 }
 
 void	printtotal(t_file **list, char *options)
 {
 	int			result;
-	t_file		*current;
 	struct stat	file;
 
-	current = *list;
 	result = 0;
-	while (current != NULL)
-	{
-		if (OPTIN(options, 'a') == NULL && 
-            TRIM(current->filename)[0] == '.' &&
-            OPTIN(options, 'A') == NULL)
-		{
-			current = current->next;
-			continue ;
-		}
-        else if (OPTIN(options, 'A') && OPTIN(options, 'a') == NULL)
-		{
-            if (TRIM(current->filename) == "." ||
-                TRIM(current->filename) == "..")
-            {
-                current = current->next;
-                continue ;
-            }
-		}
-		if (lstat(current->filename, &file) < 0)
-			return ;
-		if (S_ISLNK(file.st_mode))
-			result += file.st_blocks;
-		else
-		{
-			if (stat(current->filename, &file) < 0)
-				return ;
-			result += file.st_blocks;
-		}
-		current = current->next;
-	}
+	compute_total(list, &result, &file, options);
 	ft_putstr("total ");
 	ft_putnbr(result);
 	ft_putendl("");
@@ -164,44 +137,38 @@ void	printdirnl(char *directory, t_bool first)
 	}
 }
 
+void	apply_format(t_file **current, char *options, t_max *maxs)
+{
+	while (*current != NULL)
+	{
+		if (check_for_a(*current, options))
+		{
+			*current = (*current)->next;
+			continue ;
+		}
+		if (ft_strchr(options, 'l'))
+			print_l_format((*current)->filename, maxs, false);
+		else
+			printfile(TRIM((*current)->filename), options);
+		*current = (*current)->next;
+	}
+}
+
 void	listallfiles(t_file **list, char *options, char *directory, t_max *maxs)
 {
 	t_file		*current;
 	static int	i = 0;
 
 	current = *list;
-	if (i != 0)
+	if (i++ != 0)
 		printdirnl(directory, false);
 	if (current && current->next)
 	{
-		if (ft_strchr(options, 'l') && current->next->next != NULL)
+		if (ft_strchr(options, 'l') && current->next->next)
 			printtotal(list, options);
-		else if (ft_strchr(options, 'l') && current->next->next == NULL && OPTIN(options, 'a'))
+		else if (ft_strchr(options, 'l') &&
+				!current->next->next && OPTIN(options, 'a'))
 			printtotal(list, options);
 	}
-	while (current != NULL)
-	{
-		if (OPTIN(options, 'a') == NULL && 
-            TRIM(current->filename)[0] == '.' &&
-            OPTIN(options, 'A') == NULL)
-		{
-			current = current->next;
-			continue ;
-		}
-        else if (OPTIN(options, 'A') && OPTIN(options, 'a') == NULL)
-		{
-            if (ft_strcmp(TRIM(current->filename), ".") == 0 ||
-                ft_strcmp(TRIM(current->filename), "..") == 0)
-            {
-                current = current->next;
-                continue ;
-            }
-		}
-		if (ft_strchr(options, 'l'))
-			print_l_format(current->filename, maxs, false);
-		else
-			printfile(TRIM(current->filename), options);
-		current = current->next;
-	}
-	i++;
+	apply_format(&current, options, maxs);
 }
